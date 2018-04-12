edc534d583dde0a1bb1534073b89c85a2662faf0
gendium <acehugh@gmail.com>
2018-04-12 19:09:15 -0400

    Worked on scoreboard
    Adding functionality to scoreboard so that it can get scores and write scores and upload scores.

--- ScoreBoard.cpp
+++ ScoreBoard.cpp
@@ -4,9 +4,36 @@
 
 ScoreBoard::ScoreBoard()
 {
+	mScores = new map<string, double>();
 }
 
+bool ScoreBoard::uploadScoreBoard()
+{
 
+}
+
+void ScoreBoard::getScores()
+{
+	ifstream file;
+	string line;
+	file.open("scores.txt");
+	while (getline(file, line)) {
+		stringstream ss(line);
+
+	}
+}
+
+void ScoreBoard::displayScoreBoard()
+{
+	ifstream file;
+	string line;
+	file.open("scores.txt");
+	while (getline(file, line))
+	{
+		cout << line << "\n";
+	}
+	file.close();
+}
 ScoreBoard::~ScoreBoard()
 {
 }
--- ScoreBoard.h
+++ ScoreBoard.h
@@ -1,8 +1,18 @@
 #pragma once
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
 class ScoreBoard
 {
 public:
+	map<string, double> mScores;
 	ScoreBoard();
+	bool uploadScoreBoard();
+	void getScores();
+	void displayScoreBoard();
 	~ScoreBoard();
 };
 
